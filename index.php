7687556b7220a47c9724b8589b8b84a9d2290bf4
Ari625 <arir06101@gmail.com>
2024-01-30 11:46:56 +0700

    0.1

--- index.php
+++ index.php
@@ -1,14 +1,54 @@
+
 <!DOCTYPE html>
 <html>
 <head>
    <meta charset='utf-8'>
    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
    <title>EASY PARKIR</title>
-   <meta name='viewport' content='width=device-width, initial-scale=1'>
-   <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <script src='main.js'></script>
 </head>
 <body>
-   
+
+   <nav class="navbar bg-body-secondary shadow-sm">
+   <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">
+         <text class="bg-success text-white p-1 rounded-2">
+            EASY PARKIR
+         </text>
+      </span>
+   </div>
+   </nav>
+
+   <div class="p-3 position-absolute top-50 start-50 translate-middle">
+      <div class="card" style="height : 25rem; width: 25rem;">
+         <div class="card-header">
+            <ul class="nav justify-content-center">
+               <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">MASUK</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">KELUAR</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">LIST KENDARAAN</a>
+               </li>
+            </ul>
+         </div>
+         <div class="card-body ">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+         </div>
+         <div class="card-footer">
+            <text class="text-secondary">
+               &copy2024
+            </text>
+         </div>
+      </div>
+   </div>
+
 </body>
 </html>
