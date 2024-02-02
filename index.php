d3ea8c823250600c99f286336a9807b1528c7755
Ari625 <arir06101@gmail.com>
2024-02-02 09:27:22 +0700

    0.9

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if (isset($_POST["submitKeluar"])) {
    }
 }
 
-$listKendaraan = query("SELECT * FROM k_keluar")
+$listKendaraan = query("SELECT * FROM k_keluar ORDER BY waktu_masuk ASC")
    ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -222,6 +222,19 @@ $listKendaraan = query("SELECT * FROM k_keluar")
             <div id="listKendaraan" class="content" ">
                <!-- Konten untuk tombol list kendaraan -->
                <h4 class=" text-center">List kendaraan</h4>
+               <div class="d-flex flex-row-reverse">
+                  <a href="report.php" class="btn btn-success mb-2">
+                     <i class="bi bi-printer">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                           class="bi bi-printer" viewBox="0 0 16 16">
+                           <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1" />
+                           <path
+                              d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1" />
+                        </svg>
+                     </i>
+                     Cetak
+                  </a>
+               </div>
                <div class="overflow-x-scroll">
                   <table class="table-bordered">
                      <tr>
---
+++ report.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["login"])) {
+   header("location: login.php");
+   exit;
+}
+require "lib/fpdf184/fpdf.php";
+require "functions.php";
+
+$pdf = new FPDF("P", "mm", "A4");
+$pdf->AddPage();
+
+$pdf->SetFont("Times", "B", 10);
+$pdf->Cell(200, 10, "Data Barang", 0, 1, "C");
+
+$pdf->Cell(10, 15, '', 0, 1);
+$pdf->SetFont('Times', 'B', 9);
+$pdf->Cell(10, 7, 'NO', 1, 0, 'C');
+$pdf->Cell(30, 7, 'Plat Nomor', 1, 0, 'C');
+$pdf->Cell(40, 7, 'Waktu Masuk', 1, 0, 'C');
+$pdf->Cell(40, 7, 'Waktu Keluar', 1, 0, 'C');
+$pdf->Cell(30, 7, 'Merk', 1, 0, 'C');
+
+$pdf->Cell(10,7,'',0,1);
+$pdf->SetFont('Times','',10);
+$no=1;
+$barang = query('SELECT * FROM k_keluar');
+foreach($barang as $row){
+   $pdf->Cell(10,6, $no++,1,0,'C');
+   $pdf->Cell(30,6, $row['plat_no'],1,0);
+   $pdf->Cell(40,6, $row['waktu_masuk'],1,0);  
+   $pdf->Cell(40,6, $row['waktu_masuk'],1,0);
+   $pdf->Cell(30,6, $row['merk'],1,1);
+}
+$pdf->Output();
+// $pdf->Output("F","report/laporan.pdf");
+?>
